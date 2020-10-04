3cb003535acce96aec1d183c85d5b0ca81dc330a
Fran Perez <mr.rockss@gmail.com>
2020-10-04 23:14:51 +0200

    Add default properties

--- logic.js
+++ logic.js
@@ -3,11 +3,14 @@ const baselineGridBackground = document.getElementById("baselineGridBackground")
 
 const textSizeInput = document.getElementById("textSizeInput")
 const textSizeRange = document.getElementById("textSizeRange")
+
 const lineHeightRatioInput = document.getElementById("lineHeightRatioInput")
 const lineHeightRatioRange = document.getElementById("lineHeightRatioRange")
+
 const baselineGridInput = document.getElementById("baselineGridInput")
 const baselineGridRange = document.getElementById("baselineGridRange")
-const lineHeightDiv = document.getElementById("lineHeightDiv")
+
+const generatedLineHeight = document.getElementById("generatedLineHeight")
 
 let getTextProperties = () => {
   size = Number(textSizeInput.value)
@@ -21,7 +24,19 @@ let getTextProperties = () => {
   }
 }
 
-let calculateOffset = () => {
+let setTextProperties = (size, ratio, grid) => {
+  textSizeInput.value = size
+  lineHeightRatioInput.value = ratio
+  baselineGridInput.value = grid
+
+  textSizeRange.value = size
+  lineHeightRatioRange.value = ratio
+  baselineGridRange.value = grid
+
+  updateSample(size)
+}
+
+let calculateBaselineOffset = () => {
   let proxy = document.createElement("span")
   proxy.setAttribute("style", "font-size:0")
   proxy.innerText = "A"
@@ -49,46 +64,46 @@ let renderBaselineGrid = () => {
   }px`
 }
 
-let applyTextProperties = () => {
-  textProperties = getTextProperties()
-  lineHeight = calculateLineHeight(textProperties)
+let updateSample = () => {
+  let textProperties = getTextProperties()
+  let lineHeight = calculateLineHeight(textProperties)
 
   sample.style.top = `0`
 
   sample.style.fontSize = `${textProperties.size}px`
   sample.style.lineHeight = `${lineHeight}px`
-  sample.style.top = `-${calculateOffset()}px`
+  sample.style.top = `-${calculateBaselineOffset()}px`
 
-  lineHeightDiv.innerHTML = lineHeight
+  generatedLineHeight.innerHTML = lineHeight
 
   renderBaselineGrid()
 }
 
 textSizeInput.addEventListener("input", function (e) {
   textSizeRange.value = this.value
-  applyTextProperties(this.value)
+  updateSample(this.value)
 })
 textSizeRange.addEventListener("input", function (e) {
   textSizeInput.value = this.value
-  applyTextProperties(this.value)
+  updateSample(this.value)
 })
 
 lineHeightRatioInput.addEventListener("input", function (e) {
   lineHeightRatioRange.value = this.value
-  applyTextProperties(this.value)
+  updateSample(this.value)
 })
 lineHeightRatioRange.addEventListener("input", function (e) {
   lineHeightRatioInput.value = this.value
-  applyTextProperties(this.value)
+  updateSample(this.value)
 })
 
 baselineGridInput.addEventListener("input", function (e) {
   baselineGridRange.value = this.value
-  applyTextProperties(this.value)
+  updateSample(this.value)
 })
 baselineGridRange.addEventListener("input", function (e) {
   baselineGridInput.value = this.value
-  applyTextProperties(this.value)
+  updateSample(this.value)
 })
 
-applyTextProperties(16)
+setTextProperties(24, 1.3, 8)
