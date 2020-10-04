195894f0e9399d019a960fcf594cfec08c086f71
Fran Perez <mr.rockss@gmail.com>
2020-10-04 16:32:20 +0200

    New baseline grid

--- logic.js
+++ logic.js
@@ -1,61 +1,55 @@
 const calculateLineHeightButton = document.getElementById(
   "calculateLineHeightButton"
-);
-const sample = document.getElementById("sample");
-const container = document.getElementById("container");
+)
+const sample = document.getElementById("sample")
+const container = document.getElementById("container")
+const baselineGrid = document.getElementById("baselineGrid")
 
 let getTextSize = () => {
-  let textSize = Number(document.getElementById("textSize").value);
-  return textSize;
-};
+  let textSize = Number(document.getElementById("textSize").value)
+  return textSize
+}
 
 let getBaselineRowHeight = () => {
-  let baselineRowHeight = Number(document.getElementById("baselineRowHeight").value);
-  if (baselineRowHeight.length == 0) baselineRowHeight = 8;
-  return baselineRowHeight;
-};
+  let baselineRowHeight = Number(
+    document.getElementById("baselineRowHeight").value
+  )
+  if (baselineRowHeight.length == 0) baselineRowHeight = 8
+  return baselineRowHeight
+}
 
 let getLineHeightRatio = () => {
-  let lineHeightRatio = document.getElementById("lineHeightRatio").value;
-  if (lineHeightRatio.length == 0) lineHeightRatio = 1.3;
-  return lineHeightRatio;
-};
+  let lineHeightRatio = document.getElementById("lineHeightRatio").value
+  if (lineHeightRatio.length == 0) lineHeightRatio = 1.3
+  return lineHeightRatio
+}
 
 let calculateLineHeight = () => {
   let lineHeight =
     Math.ceil((getTextSize() * getLineHeightRatio()) / getBaselineRowHeight()) *
-    getBaselineRowHeight();
-  return lineHeight;
-};
-
-// let getCoords = (elem) => {
-//   let box = elem.getBoundingClientRect();
-
-//   return {
-//     top: box.top + window.pageYOffset,
-//     right: box.right + window.pageXOffset,
-//     bottom: box.bottom + window.pageYOffset,
-//     left: box.left + window.pageXOffset,
-//   };
-// };
+    getBaselineRowHeight()
+  return lineHeight
+}
 
 let getBaseline = (element) => {
-  let span = document.createElement("span");
-  span.setAttribute("style", "font-size:0");
-  span.innerText = "A";
-  element.insertBefore(span, element.firstChild);
+  let span = document.createElement("span")
+  span.setAttribute("style", "font-size:0")
+  span.innerText = "A"
+  element.insertBefore(span, element.firstChild)
 
-  let computed = span.getBoundingClientRect().bottom - element.offsetTop;
-  console.log(span.getBoundingClientRect().bottom, element.offsetTop)
-  span.remove();
+  let computed = span.getBoundingClientRect().bottom - element.offsetTop
 
-  return computed;
-};
+  span.remove()
+
+  return computed
+}
 
 calculateLineHeightButton.addEventListener("click", (event) => {
-  sample.style.top = `0`;
-  
-  sample.style.fontSize = `${getTextSize()}px`;
-  sample.style.lineHeight = `${calculateLineHeight()}px`;
-  sample.style.top = `-${getBaseline(sample) - getBaselineRowHeight()}px`;
-});
+  sample.style.top = `0`
+
+  sample.style.fontSize = `${getTextSize()}px`
+  sample.style.lineHeight = `${calculateLineHeight()}px`
+  sample.style.top = `-${
+    getBaseline(sample) - getBaselineRowHeight()
+  }px`
+})
